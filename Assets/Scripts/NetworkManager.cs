ce2115905ed70b1a430c06b9eb1a93c9999616e5
MNiekel <maartje@niekel.net>
2015-04-16 16:02:18 +0200

    First commit

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+
+	public Camera worldCamera;
+
+	private SpawnPoint[] spawnPoints;
+
+	// Use this for initialization
+	void Start () {
+		spawnPoints = FindObjectsOfType <SpawnPoint> ();
+		if (spawnPoints == null) {
+			Debug.LogError ("No Spawnpoints!");
+		}
+		Connect ();
+	}
+
+	void Connect () {
+		//PhotonNetwork.offlineMode = true;
+		PhotonNetwork.ConnectUsingSettings ("MultiplayerFPS v0.0.1");
+	}
+
+	void OnGUI () {
+		GUILayout.Label (PhotonNetwork.connectionStateDetailed.ToString ());
+	}
+
+	void OnJoinedLobby () {
+		Debug.Log ("Joined lobby");
+		PhotonNetwork.JoinRandomRoom ();
+	}
+
+	void OnPhotonRandomJoinFailed () {
+		Debug.Log ("Joining room failed, creating new room");
+		PhotonNetwork.CreateRoom (null);
+	}
+
+	void OnJoinedRoom () {
+		Debug.Log ("Joined room");
+
+		SpawnPlayer ();
+	}
+
+	void SpawnPlayer () {
+		SpawnPoint spawnPoint = spawnPoints [Random.Range(0, spawnPoints.Length)];
+		PhotonNetwork.Instantiate ("Player Controller",
+		           spawnPoint.transform.position, spawnPoint.transform.rotation, 0);
+		worldCamera.enabled = false;
+	}
+}
