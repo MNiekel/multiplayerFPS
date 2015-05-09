c5c19de0390325a57958d564357607f8622f474e
MNiekel <maartje@niekel.net>
2015-05-09 08:17:35 +0200

    AI Bot now only shoots when a player is in the line of fire. Does not take into account blocking of sight.

--- Assets/Scripts/BotShooting.cs
+++ Assets/Scripts/BotShooting.cs
@@ -4,23 +4,30 @@ using System.Collections;
 public class BotShooting : MonoBehaviour {
 
 	private NetworkCharacter networkCharacter;
-
-	private float shotTimer;
 	
 	void Start () {
 		networkCharacter = GetComponent <NetworkCharacter> ();
-		shotTimer = 2f; //Random.Range (3f, 5f);
 	}
 	
 	void Update () {
-		
-		shotTimer -= Time.deltaTime;
 
-		if (shotTimer < 0) {
-			networkCharacter.isShooting = true;
-			shotTimer = Random.Range (0.5f, 2.5f);
-		} else {
-			networkCharacter.isShooting = false;
+		networkCharacter.isShooting = EvaluateShooting ();
+	}
+
+	private bool EvaluateShooting () {
+
+		Ray ray = new Ray (transform.position, transform.forward);
+		
+		RaycastHit [] hits = Physics.RaycastAll (ray);
+		
+
+		for (int i = 0; i < hits.Length; i++) {
+			if (hits [i].collider.tag == "Player") {
+				return true;
+			}
 		}
+
+		return false;
 	}
 }
+
